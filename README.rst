55932f34de322af182d6efe9c5fd83f7dac29e07
rudolfspetrovs <rudolfs.petrovs@lu.lv>
2019-04-08 19:50:30 +0100

    Created package structure, added some of the required info

---
+++ README.rst
@@ -0,0 +1,24 @@
+VizAn
+=====
+
+-----
+
+.. contents:: **Table of Contents**
+    :backlinks: none
+
+Installation
+------------
+
+VizAn is distributed on `PyPI <https://pypi.org>`_ as a universal
+wheel and is available on Linux/macOS and Windows and supports
+Python 2.7/3.5+ and PyPy.
+
+.. code-block:: bash
+
+    $ pip install VizAn
+
+License
+-------
+
+VizAn is distributed under the terms of `GPL v3 License <https://choosealicense.com/licenses/gpl-3.0/>`_
+
---
+++ setup.py
@@ -0,0 +1,52 @@
+from io import open
+
+from setuptools import find_packages, setup
+
+with open('vizan/__init__.py', 'r') as f:
+    for line in f:
+        if line.startswith('__version__'):
+            version = line.strip().split('=')[1].strip(' \'"')
+            break
+    else:
+        version = '0.1.0'
+
+with open('README.rst', 'r', encoding='utf-8') as f:
+    readme = f.read()
+
+REQUIRES = ['pysvg-py3>=0.2.2.post2', 'cobra>=0.14.2']
+
+setup(
+    name='vizan',
+    version=version,
+    author='Agris Pentjuss',
+    author_email='agrosh@inbox.lv',
+    maintainer='Rudolfs Petrovs',
+    maintainer_email='rudolfs.petrovs@lu.lv',
+    description='',
+    long_description=readme,
+    long_description_content_type="text/x-rst",
+    url='https://github.com/lv-csbg/vizan',
+    license='GPLv3',
+
+    keywords=[
+        '',
+    ],
+
+    classifiers=[
+        'Development Status :: 3 - Alpha'
+        'Intended Audience :: Science/Research',
+        'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
+        'License :: FSF Approved :: GNU General Public License v3 (GPLv3)',
+        'Natural Language :: English',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Topic :: Scientific/Engineering :: Bio-Informatics',
+    ],
+
+    install_requires=REQUIRES,
+    tests_require=['coverage', 'pytest'],
+
+    packages=find_packages(),
+)
