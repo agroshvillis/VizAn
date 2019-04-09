3a89037b6de59023df8156b0ab066ce826a272f3
rudolfspetrovs <rudolfs.petrovs@lu.lv>
2019-04-09 17:27:25 +0100

    Added use of absolute pathway, put suggested options to setup

--- setup.py
+++ setup.py
@@ -1,12 +1,13 @@
 import sys
 from io import open
-
+from os import path
 from setuptools import find_packages, setup
 
 CURRENT_PYTHON = sys.version_info[:2]
+
 if CURRENT_PYTHON == (2, 7):
     pysvg = 'pysvg>=0.2.2'
-elif CURRENT_PYTHON >= (3, 5):
+elif CURRENT_PYTHON >= (3, 4):
     pysvg = 'pysvg-py3>=0.2.2.post2'
 
 required = [
@@ -14,7 +15,9 @@ required = [
     'cobra>=0.14.2'
 ]
 
-with open('vizan/__init__.py', 'r') as f:
+here = path.abspath(path.dirname(__file__))
+
+with open(path.join(here, 'vizan/__init__.py'), 'r') as f:
     for line in f:
         if line.startswith('__version__'):
             version = line.strip().split('=')[1].strip(' \'"')
@@ -22,7 +25,7 @@ with open('vizan/__init__.py', 'r') as f:
     else:
         version = '0.1.0'
 
-with open('README.rst', 'r', encoding='utf-8') as f:
+with open(path.join(here, 'README.rst'), 'r', encoding='utf-8') as f:
     readme = f.read()
 
 setup(
@@ -32,15 +35,13 @@ setup(
     author_email='agrosh@inbox.lv',
     maintainer='Rudolfs Petrovs',
     maintainer_email='rudolfs.petrovs@lu.lv',
-    description='',
+    description='VizAn is a visualization tool for Flux balance analysis (FBA) and Flux variability analysis (FVA).',
     long_description=readme,
     long_description_content_type="text/x-rst",
     url='https://github.com/lv-csbg/vizan',
     license='GPLv3',
 
-    keywords=[
-        '',
-    ],
+    keywords='FBA FVA',
 
     classifiers=[
         'Development Status :: 3 - Alpha'
@@ -58,5 +59,6 @@ setup(
     install_requires=required,
     tests_require=['coverage', 'pytest'],
 
-    packages=find_packages(),
+    packages=find_packages(exclude=['docs', 'tests']),
+    python_requires='>=2.7, !=3.0.*, !=3.1.*, !=3.2.*, !=3.3.*, <4',
 )
